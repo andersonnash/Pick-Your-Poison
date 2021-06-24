b765cb2b4bb34126f894369f7e373bef22fa9e54
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-06-24 14:43:20 -0600

    done with my MVP

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('button')
+const button2 = document.querySelector('button')
 const section = document.querySelector('.your-drink')
 const dataContainer = document.querySelector("#data-container")
-console.log(dataContainer)
+// console.log(dataContainer)
 
 
 async function fetchData(name) {
@@ -9,42 +10,48 @@ async function fetchData(name) {
     // const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
     const response = await axios.get(url)
-    console.log(response)
-    console.log(response.data.drinks)
+    // console.log(response)
+    // console.log(response.data.drinks)
     const drinksArray = Object.values(response.data.drinks)
-    console.log(drinksArray)
+    // console.log(drinksArray)
     showDrinkData(drinksArray)
     return response
   } catch (error) {
     console.error(error)
     let errorMsg = document.createElement('h1')
-    errorMsg.textContent = 'Why Are You Still Awake?'
+    errorMsg.textContent = 'Check Spelling and Try Again!'
     dataContainer.append(errorMsg)
   }   
 }
+// fetchData('gin')
+
+
 
 async function alcData(name) {
   try {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`
+    // console.log(url)
     const response = await axios.get(url)
-    console.log(response)
-    console.log(response.data.drinks)
-    // const drinksArray = Object.values(response.data.drinks)
-    // console.log(drinksArray)
-    // showDrinkData(drinksArray)
-    // return response
+    // console.log(response)
+    // console.log(response.data.drinks)
+    const alcsArray = Object.values(response.data.drinks)
+    // console.log(alcsArray)
+    showAlcData(alcsArray)
+    return response
   } catch (error) {
     console.error(error)
   }   
 }
-alcData('gin')
+// alcData('vodka')
 
 function showAlcData(data){
   for (let i = 0; i < data.length; i++) {
+    console.log(data[i])
     const alcDiv = document.createElement('div')
+    alcDiv.classList.add("alvDiv")
     section.append(alcDiv)
 
-    const alcName = createElement('h3')
+    const alcName = document.createElement('h3')
     alcName.textContent = `${data[i].strDrink}`
     console.log(data[i].strDrink)
     alcDiv.append(alcName)
@@ -53,13 +60,9 @@ function showAlcData(data){
     drinksImg.setAttribute('src', data[i].strDrinkThumb)
     console.log(data[i].strDrinkThumb)
     drinksImg.classList.add('drinksImg')
-    drinkDiv.append(drinksImg)
-
-
+    alcDiv.append(drinksImg)
   }
 }
-
-// (! || '')
 
 function showDrinkData(data) {
   for (let i = 0; i < data.length; i++) {
@@ -72,70 +75,15 @@ function showDrinkData(data) {
 
     const drinkName = document.createElement('h3')
     drinkName.textContent = `${data[i].strDrink}`
-    console.log(data[i].strDrink)
+    // console.log(data[i].strDrink)
     drinkDiv.append(drinkName)
-
-    // const drinkPic = document.createElement('img')
-    // drinkPic.setAttribute('src', data[i].imageURL)
-    // console.log(data[i].imageURL)
-    // // console.log(drinkPic)
 
     const drinksImg = document.createElement('img')
     drinksImg.setAttribute('src', data[i].strDrinkThumb)
     // < img; src = `${data.strDrinkThumb}`;style ="height: 250px; width: auto">
-    console.log(data[i].strDrinkThumb)
+    // console.log(data[i].strDrinkThumb)
     drinksImg.classList.add('drinksImg')
     drinkDiv.append(drinksImg)
-
-    //Create empty array for measurments 
-    let measurments = []
-
-    //Create empty array for ingredients
-    let ingredients = []
-
-    //Push measurments into the measurments array with a for in loop - inside of loop use control flor - if statment make sure it has.includes() str
-    // for (measurments in array) {
-    //   if (measurments === strMeasure && measurments) includes; " " || '';
-    // }
-    //Push ingredients into the ingredients array with a for in loop
-
-    //For Loop (i < measurmenets.length) 
-    //create a p element within the for loop - textContent of ele use `${}`
-    //ingredients[i] 
-    //measurments[i]
-
-
-
-
-    const ing1 = document.createElement('p')
-    ing1.textContent = `${data[i].strIngredient1}:`
-    console.log(data[i].strIngredient1)
-    drinkDiv.append(ing1)
-
-    const ing2 = document.createElement('p')
-    ing2.textContent = `${data[i].strIngredient2}:`
-    console.log(data[i].strIngredient2)
-    drinkDiv.append(ing2)
-
-    const ing3 = document.createElement('p')
-    ing3.textContent = `${data[i].strIngredient3}:`
-    console.log(data[i].strIngredient3)
-    drinkDiv.append(ing3)
-
-    const ing4 = document.createElement('p')
-    ing4.textContent = `${data[i].strIngredient4}:`
-    console.log(data[i].strIngredient4)
-    drinkDiv.append(ing4)
-
-    // const ing5 = document.createElement('p')
-    // ing5.textContent = `${data[i].strIngredient5}:`
-    // console.log(data[i].strIngredient5)
-    // drinkDiv.append(ing5)
-
-    // const ing6 = document.createElement('p')
-    // ing6.textContent = `${data[i].strIngredient6}:`
-    // console.log(data[i].strIngredient6)
-    // drinkDiv.append(ing6)
 
     const inst = document.createElement('h4')
     inst.textContent = `${data[i].strInstructions}`
@@ -143,74 +91,59 @@ function showDrinkData(data) {
     // inst.classList.add('inst')
     drinkDiv.append(inst)
 
+    // Create empty array for measurments 
+    // Create empty array for ingredients
+      let measurments = []
+      let ingredients = []
 
-    const meas1 = document.createElement('li')
-    meas1.textContent = `${data[i].strMeasure1}`
-    console.log(data[i].strMeasure1)
-    ing1.append(meas1)
+      //Push measurments into the measurments array with a for in loop - inside of loop use control flor - if statment make sure it has.includes() str
+      for (let measurment in data[i]) {
+        if (measurment.includes("strMeasure") && data[i][measurment] != null) {
+          measurments.push(data[i][measurment])
+        }
+      }
 
-    const meas2 = document.createElement('li')
-    meas2.textContent = `${data[i].strMeasure2}`
-    console.log(data[i].strMeasure2)
-    ing2.append(meas2)
-
-    const meas3 = document.createElement('li')
-    meas3.textContent = `${data[i].strMeasure3}`
-    console.log(data[i].strMeasure3)
-    ing3.append(meas3)
-
-    const meas4 = document.createElement('li')
-    meas4.textContent = `${data[i].strMeasure4}`
-    console.log(data[i].strMeasure4)
-    ing4.append(meas4)
-
-
-
-    // return !value
-    // if (strIngredient6 === null) {
-    //   console.log('')
-    // } else {
-    //   console.log(strIngredient6)
-    // }
+    for (let ingredient in data[i]) {
+      console.log(data[i][ingredient])
+        if (ingredient.includes('strIngredient') &&  data[i][ingredient] != null) {
+          ingredients.push(data[i][ingredient])  
+      }
+    }
+    for (let i = 0; i < ingredients.length; i++){
+      const ing = document.createElement('p')
+      ing.textContent = `${ingredients[i]}: ${measurments[i]}`
+      drinkDiv.append(ing)
+    }
+    console.log(ingredients)
+    console.log(measurments)
+  }
 
   }
-}
+  button.addEventListener('click', () => {
+    // const inputValue = document.querySelector('#blank').value
+    let data = document.querySelector('#blank').value
+    // console.log(data)
+    
+    fetchData(data)
+    // alcData(data)
+    removeDrink()
+    return data
+  })
 
 
-// const form = document.querySelector("#drink-form")
+button2.addEventListener('click', () => {
+    // e.preventDefault
+    // const inputValue = document.querySelector('#blank').value
+    const data = document.querySelector('#blank2').value
+    // console.log(data)
+    alcData(data)
+    removeDrink()
+    return data
+  })
 
-// form.addEventListener('click', (e) => {
-//   e.preventDefault()
-//   console.log(inputValue)
-
-//   // removeCountry(dataContainer)
-//   fetchData(inputValue)
-//   document.querySelector('#drink-search').value = ''
-// })
-
-button.addEventListener('click', () => {
-  // const inputValue = document.querySelector('#blank').value
-  let data = document.querySelector('#blank').value
-  console.log(data)
-  fetchData(data)
-  // alcData(data)
-  removeDrink()
-  return data
-})
-
-// button.addEventListener('submit', () => {
-//   // const inputValue = document.querySelector('#blank').value
-//   let data = document.querySelector('#blank2').value
-//   console.log(data)
-//   // fetchData(data)
-//   alcData(data)
-//   // removeDrink()
-//   return data
-// })
-
-function removeDrink() {
-  while (section.lastChild) {
-    section.removeChild(section.lastChild)
+  function removeDrink() {
+    while (section.lastChild) {
+      section.removeChild(section.lastChild)
+    }
   }
-}
 
